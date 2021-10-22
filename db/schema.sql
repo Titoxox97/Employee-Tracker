c98011457cb4a9a37cdb14efd73b03c474ab6ad9
Titoxox97 <87781987+Titoxox97@users.noreply.github.com>
2021-10-21 22:50:53 -0400

    schema

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employee_manager;
+CREATE DATABASE employee_manager;
+
+USE employee_manager;
+
+CREATE TABLE deparment(
+  id INT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL, 
+);
+
+CREATE TABLE role(
+  id INT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id),
+  REFERENCES department(id),
+  ON DELETE CASCADE,
+);
+
+CREATE TABLE employee(
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (manager_id),
+    REFERENCES role(id),
+    ON DELETE CASCADE,
+);
