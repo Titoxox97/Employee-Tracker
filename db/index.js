f5808bb1667753c7d7aba8b8bd65b60326270df2
Titoxox97 <87781987+Titoxox97@users.noreply.github.com>
2021-10-25 20:56:04 -0400

    prompt functions

--- db/index.js
+++ db/index.js
@@ -1,29 +1,27 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
-// connect the file to the database
-const connection = mysql2.createConnection(
-  {
-    host: "localhost",
-    user: "root",
-    password: "Glevy9897Ahrens",
-    database: "employee_manager",
-  },
-  console.log("Database is connected")
-);
+// initialize the project
+function initialize() {
+  runPromptInq();
+}
+initialize();
 
-const controlCenter = [
-  {
-    type: "list",
-    message: "Where do you want to go?",
-    name: "Control Center",
-    choices: [
-      "view all branches",
-      "view all positions",
-      "view all associates",
-      "add a branch",
-      "add an associate",
-      "update an associates position",
-    ],
-  },
-];
+// Prompt functions
+
+function runPromptInq() {
+  inquirer.prompt().then((response) => getQueries(response));
+}
+
+function getQueries(response) {
+  switch (true) {
+    case response.navigation === "view all branches":
+      db.query("SELECT * FROM departments", function (err, results) {
+        console.table(results);
+        runPromptInq();
+      });
+      break;
+    case response.navigation === "view all positions":
+      db.query();
+  }
+}
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const connection = require("./db/connection");
 inquire
   .prompt([
     {
-      //   change value names
       type: "list",
       name: "choice",
       message: "What would you like to do?",
@@ -15,71 +14,149 @@ inquire
         },
         {
           name: "View All Employees By Department",
-          value: "VIEW_EMPLOYEES_BY_DEPARTMENT",
+          value: "viewEmployees_by_branch",
         },
         {
           name: "View All Employees By Manager",
-          value: "VIEW_EMPLOYEES_BY_MANAGER",
+          value: "viewEmployees_by_manager",
         },
         {
           name: "Add Employee",
-          value: "ADD_EMPLOYEE",
+          value: "add_employee",
         },
         {
           name: "Remove Employee",
-          value: "REMOVE_EMPLOYEE",
+          value: "remove_employee",
         },
         {
           name: "Update Employee Role",
-          value: "UPDATE_EMPLOYEE_ROLE",
+          value: "update_employee_position",
         },
         {
           name: "Update Employee Manager",
-          value: "UPDATE_EMPLOYEE_MANAGER",
+          value: "update_employee_manager",
         },
         {
           name: "View All Roles",
-          value: "VIEW_ROLES",
+          value: "view_positions",
         },
         {
           name: "Add Role",
-          value: "ADD_ROLE",
+          value: "add_position",
         },
         {
           name: "Remove Role",
-          value: "REMOVE_ROLE",
+          value: "remove_position",
         },
         {
           name: "View All Departments",
-          value: "VIEW_DEPARTMENTS",
+          value: "view_branches",
         },
         {
           name: "Add Department",
-          value: "ADD_DEPARTMENT",
+          value: "add_branch",
         },
         {
           name: "Remove Department",
-          value: "REMOVE_DEPARTMENT",
+          value: "remove_branch",
         },
         {
           name: "View Total Utilized Budget By Department",
-          value: "VIEW_UTILIZED_BUDGET_BY_DEPARTMENT",
+          value: "view_budget_by_branch",
         },
         {
           name: "Quit",
-          value: "QUIT",
+          value: "quit",
         },
       ],
     },
   ])
+  //   use similar function for all prompts
   .then((res) => {
     var answer = res.choice;
+    console.log(answer);
     if (answer === "viewEmployees") {
-      var result = connection
-        .promise()
-        .query("SELECT * FROM employees")
-        .then((res) => {
-          console.table(res);
-        });
+      connection.query("SELECT * FROM employees", function (err, res) {
+        if (err) console.log(err);
+        console.table(res);
+      });
+    }
+    if (answer === "viewEmployees_by_branch") {
+      connection.query("SELECT * FROM employees", function (err, res) {
+        if (err) console.log(err);
+        console.table(res);
+      });
+    }
+    if (answer === "viewEmployees_by_manager") {
+      connection.query("SELECT * FROM employees", function (err, res) {
+        if (err) console.log(err);
+        console.table(res);
+      });
+    }
+    if (answer === "add_employee") {
+      connection.query("SELECT * FROM employees", function (err, res) {
+        if (err) console.log(err);
+        console.table(res);
+      });
+    }
+    if (answer === "remove_employee") {
+      connection.query("SELECT * FROM employees", function (err, res) {
+        if (err) console.log(err);
+        console.table(res);
+      });
+    }
+    if (answer === "update_employee_position") {
+      connection.query("SELECT * FROM role", function (err, res) {
+        if (err) console.log(err);
+        console.table(res);
+      });
+    }
+    if (answer === "update_employee_manager") {
+      connection.query("SELECT * FROM employees", function (err, res) {
+        if (err) console.log(err);
+        console.table(res);
+      });
+    }
+    if (answer === "view_positions") {
+      connection.query("SELECT * FROM role", function (err, res) {
+        if (err) console.log(err);
+        console.table(res);
+      });
+    }
+    if (answer === "add_position") {
+      connection.query("SELECT * FROM role", function (err, res) {
+        if (err) console.log(err);
+        console.table(res);
+      });
+    }
+    if (answer === "remove_position") {
+      connection.query("SELECT * FROM role", function (err, res) {
+        if (err) console.log(err);
+        console.table(res);
+      });
+    }
+    if (answer === "view_branches") {
+      connection.query("SELECT * FROM department", function (err, res) {
+        if (err) console.log(err);
+        console.table(res);
+      });
+    }
+    if (answer === "add_branch") {
+      connection.query("SELECT * FROM department", function (err, res) {
+        if (err) console.log(err);
+        console.table(res);
+      });
+    }
+    if (answer === "remove_branch") {
+      connection.query("SELECT * FROM department", function (err, res) {
+        if (err) console.log(err);
+        console.table(res);
+      });
+    }
+    if (answer === "view_budget_by_branch") {
+      connection.query("SELECT * FROM department", function (err, res) {
+        if (err) console.log(err);
+        console.table(res);
+      });
     }
   });
